1ae167f7236b795d8c47f2c0b720bd9065b80132
Skeeter Man <Michael_Newman@Yamaha-Motor.com>
2025-02-13 21:16:05 -0600

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var date = new TestViewModel
+            {
+                Name = "Student",
+                DateOfBirth= DateTime.Now
+            };
+            return View(date);
+        }
+    }
+}
