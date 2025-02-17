d45fe75d631108395e2fade037db7781e03e59d4
Michael_Newman <Michael_newman@yamaha-motor.com>
2025-02-17 16:17:18 -0600

    Completed Leave Management Module

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        [Key]
+        public int LeaveTypeId { get; set; }
+        [MaxLength(150)]
+        public string Name { get; set; } = string.Empty;
+        public int NumberOfDays { get; set; }
+
+    }
+}
